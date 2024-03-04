4cd031b0e2180046174185e4eb4130b04c549c6e
sehrish22 <sp21-bcs-089@cuilahore.edu.pk>
2024-03-04 09:09:37 +0500

    uptolecture6

--- JavaScrpt/6eventbinding.js
+++ JavaScrpt/6eventbinding.js
@@ -2,3 +2,10 @@ console.log("Event binding practice");
 function btnclick(){
     console.log("I am Clicked");
 }
+function update(){
+    console.log('updated');
+    var input = document.getElementById("myinput");
+    console.log(input.value);
+    var output = document.getElementById("output");
+    output.innerHTML=input.value;
+}
