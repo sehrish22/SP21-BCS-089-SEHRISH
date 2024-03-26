7cfe3fe7c6e2ca8c678ff98d19e96b485f673a9e
sehrish22 <sp21-bcs-089@cuilahore.edu.pk>
2024-03-26 22:31:38 +0500

    landingfinal

--- jquery/lecture13.js
+++ jquery/lecture13.js
@@ -1,31 +1,46 @@
 $(function () {
-    loadproducts();
-  });
-  function loadproducts() {
-    $.ajax({
-      url: "https://usmanlive.com/wp-json/api/stories",
-      method: "GET",
-      success: function (response) {
-        console.log(response);
-        var products = $("#products");
-        products.empty();
-        //   for (var i = 0; i < response.length; i++) {
-        //     products.append("<div><h4>title of product</h4></div>");
-        //   }
-        for (var i = 0; i < response.length; i++) {
-          var produ = response[i];
-          products.append("<div><h4>" + produ.title + "</h4></div>");
-        }
+  loadproducts();
+  $("#products").on("click", ".btn-danger", deleteproducts);
+});
+function loadproducts() {
+  $.ajax({
+    url: "https://usmanlive.com/wp-json/api/stories",
+    method: "GET",
+    success: function (response) {
+      var products = $("#products");
+      products.empty();
+      //   for (var i = 0; i < response.length; i++) {
+      //     products.append("<div><h4>title of product</h4></div>");
+      //   }
+      for (var i = 0; i < response.length; i++) {
+        var produ = response[i];
+        products.append("<div><h4>" + produ.title + "</h4></div>");
+      }
+      for (var i = 0; i < response.length; i++) {
+        var prod = response[i];
         for (var i = 0; i < response.length; i++) {
           var prod = response[i];
-          for (var i = 0; i < response.length; i++) {
-            var prod = response[i];
-            products.append(
-              `<div class="produc" data-id="${prod._id}"><h4>${prod.title}</h4><p>${prod.content}<button class="btn btn-warning btn-xl float-right">EDIT</button><button class="btn btn-danger btn-xl float-right">DELETE</button></p></div>`
-            );
-          }
+          products.append(
+            `<div class="produc" data-id="${prod.id}"><h4>${prod.title}</h4><p>${prod.content}<button class="btn btn-warning btn-xl float-right">EDIT</button><button class="btn btn-danger btn-xl float-right">DELETE</button></p></div>`
+          );
         }
-      },
-    });
-  }
-  
+      }
+    },
+  });
+}
+function deleteproducts() {
+  //brings the tag(of button) which is clicked
+  var btnn = $(this);
+  //storing div on whose button is clicked and stored in btnn
+  var parDiv = btnn.closest(".produc");
+  //getting id of that button
+  let id = parDiv.attr("data-id");
+  console.log(id);
+  $.ajax({
+    url: "https://usmanlive.com/wp-json/api/stories" + id,
+    method: "DELETE",
+    success: function () {
+      loadproducts();
+    },
+  });
+}
