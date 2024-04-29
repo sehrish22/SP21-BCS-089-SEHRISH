4a54458d8d20e35d8aa51342238c0c27cf728f2c
sehrish22 <sp21-bcs-089@cuilahore.edu.pk>
2024-04-29 12:23:42 +0500

    node..

--- node/lecture22.js
+++ node/lecture22.js
@@ -1,8 +1,17 @@
 const http = require("http");
 http
   .createServer(function (req, res) {
-    console.log(req.url);
-    res.write("<h1>hello</h1>");
+    switch (req.url) {
+      case "/":
+        res.write("<h1>hello</h1>");
+        break;
+      case "/hobbies":
+        res.write("<h1>my hobbies are eating sleeping</h1>");
+        break;
+      default:
+        res.write("<h1>page not found</h1>");
+        break;
+    }
     res.end();
   })
   .listen(8080, () => {
